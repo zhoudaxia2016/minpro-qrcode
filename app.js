935ec85d31d6b2ec964d93295b0e1ef6290e8310
zhoudaxia <muzhouz@gmail.com>
2019-09-04 14:42:34 +0800

    feat: add limited

--- app.js
+++ app.js
@@ -6,13 +6,24 @@ const app = express()
 const port = 5000
 
 app.get('/', async (req, res) => {
-  let data = await getQrcode({ scene: qs.stringify(req.query) })
+  let query = req.query
+  let path, unlimited
+  if (query.path) {
+    path = query.path
+    delete query.path
+  }
+  if (query.unlimited) {
+    unlimited = query.unlimited == 1
+    delete query.unlimited
+  }
+  let data = await getQrcode(req.query, path, unlimited)
   res.end(data)
 })
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
 const url = {
   getAccessToken: 'https://api.weixin.qq.com/cgi-bin/token',
+  getWxacodeLimited: 'https://api.weixin.qq.com/wxa/getwxacode',
   getWxacodeUnlimited: 'https://api.weixin.qq.com/wxa/getwxacodeunlimit'
 }
 const name = 'parent'
@@ -28,13 +39,23 @@ const name = 'parent'
 */
 const config = require('./config').find(_ => _.name = name)
 
-async function getQrcode (params) {
+async function getQrcode (params, path = '', unlimited = false) {
   let data = await request(url.getAccessToken + '?' + qs.stringify({ grant_type: 'client_credential', appid: config.id, secret: config.secret })).catch(err => console.log(err))
   let access_token = JSON.parse(data).access_token
+  let uri, body
+  console.log('unlimited', unlimited)
+  if (unlimited) {
+    uri = url.getWxacodeUnlimited
+    body = { scene: qs.stringify(params) }
+  } else {
+    uri = url.getWxacodeLimited
+    body = { path: `${path}?${qs.stringify(params)}` }
+  }
+  console.log(body)
   data = await request({
-    uri: url.getWxacodeUnlimited + `?access_token=${access_token}`,
+    uri: uri + `?access_token=${access_token}`,
     method: 'POST',
-    body: params,
+    body,
     json: true
   })
   /*
@@ -42,5 +63,6 @@ async function getQrcode (params) {
     console.log(err)
   })
   */
+  console.log(data)
   return data
 }
